a208af66b163d06d634bc95f6e1138486e9208c1
Mayurkumar Patel <mayurt20@gmail.com>
2018-09-04 17:50:46 +0530

    Complete chapter 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header.js';
+import Contact from './components/Contact.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Lets Begin</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="Mayur" phone="+91 987 654 3210" email="mayur@gmail.com"/>
+          <Contact name="Parth" phone="+91 333 222 1111" email="parth@gmail.com"/>
+        </div>
       </div>
     );
   }
